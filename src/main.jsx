af43f0ba8dd0f5c7bfde588d4b89b7772679510b
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-08-30 13:54:17 +0200

    fixing basename

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,9 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+],{
+  basename: "/VibeStreamApp"
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
