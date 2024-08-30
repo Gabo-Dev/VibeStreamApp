da68e3ab79d223e3e5500292b10a98ed376e4af5
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-08-30 13:37:46 +0200

    fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VibeStreamApp/',
+  base: '/VibeStreamApp',
   plugins: [react()],
 })
